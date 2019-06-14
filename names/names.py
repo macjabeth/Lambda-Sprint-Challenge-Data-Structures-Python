4c5e006f1c5c84bf39e6cb07c1d443e389fb87f6
Jonathan Picazo <macbethjonathan@gmail.com>
2019-06-14 13:02:14 -0400

    complete stretch assignment

--- names/names.py
+++ names/names.py
@@ -2,7 +2,7 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r') # List containing 10000 names
+f = open('names_1.txt', 'r')  # List containing 10000 names
 names_1 = {}
 for name_1 in f.read().split("\n"):
     names_1[name_1] = True
@@ -18,5 +18,5 @@ for name_2 in names_2:
         duplicates.append(name_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
